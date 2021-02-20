12d8ed8b1236bee4cd1ef04ce462b54d33099e43
SiegChoys <67082446+SiegChoys@users.noreply.github.com>
2021-02-20 13:16:17 +0900

    Add files via upload

---
+++ src/phone_book/PhoneCompanyInfo.java
@@ -0,0 +1,18 @@
+package phone_book;
+
+public class PhoneCompanyInfo extends PhoneInfo {
+	String company;
+	
+	public PhoneCompanyInfo(String name, String num, String company)
+	{
+		super(name, num);
+		this.company=company;
+	}
+	
+	public void showPhoneInfo()
+	{
+		super.showPhoneInfo();
+		System.out.println("company : " +company);
+	}
+	
+}
---
+++ src/phone_book/PhoneInfo.java
@@ -0,0 +1,34 @@
+package phone_book;
+
+import java.util.Scanner;
+
+public class PhoneInfo {
+		String name;
+		String phoneNumber;
+		String birth;
+		
+	public PhoneInfo(String name, String num, String birth)
+	{
+		this.name=name;
+		phoneNumber=num;
+		this.birth=birth;
+	}
+		
+	public PhoneInfo(String name, String num)
+	{
+		this.name=name;
+		phoneNumber=num;
+		this.birth=null;
+	}
+		
+		public void showPhoneInfo()
+		{
+		System.out.println("name : " +name);
+		System.out.println("phone : " +phoneNumber);
+		if(birth!=null)
+			System.out.println("birth : " +birth);
+		
+		System.out.println("");	// 출력 되는 데이터의 구분을 위해
+	}
+		
+}
